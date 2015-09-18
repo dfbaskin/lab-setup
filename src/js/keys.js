213067cc9612a79efd9bbab191686f2b3ddac26c
Dave Baskin <dbaskin@wintellect.com>
2015-09-18 12:56:34 -0400

    Adding key update.

--- src/js/keys.js
+++ src/js/keys.js
@@ -1,3 +1,33 @@
 
-console.log("Keys!");
+(function() {
 
+    window.addEventListener("load", onLoaded, false);
+
+    function onLoaded() {
+
+        window.removeEventListener("load", onLoaded, false);
+
+        var request = new XMLHttpRequest();
+        request.open('GET', '/my-lab-keys', true);
+
+        request.onload = function() {
+            if (request.status >= 200 && request.status < 400) {
+                var data = JSON.parse(request.responseText);
+                var elem;
+                elem = document.getElementById('key1');
+                elem.value = data.azure.key1;
+                elem = document.getElementById('key2');
+                elem.value = data.azure.key2;
+            } else {
+                alert('Could not read lab keys!');
+            }
+        };
+
+        request.onerror = function() {
+            alert('Error reading lab keys!');
+        };
+
+        request.send();
+    }
+
+}());
